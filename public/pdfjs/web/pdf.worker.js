5a16de075425cfc587c3bfa452e3bfe689b54a94
meisty <shaundixon666@gmail.com>
2024-09-17 09:21:02 +0100

    [LA-22574] Set IsEvalSupported to false

--- public/pdfjs/web/pdf.worker.js
+++ public/pdfjs/web/pdf.worker.js
@@ -16631,7 +16631,7 @@ var PartialEvaluator = function PartialEvaluatorClosure() {
     disableFontFace: false,
     nativeImageDecoderSupport: _util.NativeImageDecoding.DECODE,
     ignoreErrors: false,
-    isEvalSupported: true
+    isEvalSupported: false
   };
   function NativeImageDecoder(_ref) {
     var xref = _ref.xref,
